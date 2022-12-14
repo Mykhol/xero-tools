ba809c930150b1ce4ede93c483d163d34caa7a59
Michael Howell <mh.michaelhowell@gmail.com>
2022-12-14 19:50:55 +1300

    Add and init NextAuth.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
 export default function Home() {
-  return <p>Hello World!</p>;
+  return <p onClick={() => signIn("google")}>Hello World!</p>;
 }
---
+++ src/lib/auth/AuthProvider.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { FC } from "react";
+import { SessionProvider } from "next-auth/react";
+
+const AuthProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+export default AuthProvider;
